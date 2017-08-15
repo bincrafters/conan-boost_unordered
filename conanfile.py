8b87fa3749dbe442f09c84201a4c1eff6c9a3bcc
Jerry W <jerrywiltse@gmail.com>
2017-08-15 16:16:43 -0400

    mass update - change short_name to array - also add user_info for generator

--- conanfile.py
+++ conanfile.py
@@ -7,7 +7,7 @@ class BoostUnorderedConan(ConanFile):
     source_url = "https://github.com/boostorg/unordered"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_name = "unordered"
+    lib_short_names = ["unordered"]
     requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
                       "Boost.Config/1.64.0@bincrafters/testing", \
                       "Boost.Container/1.64.0@bincrafters/testing", \
@@ -26,12 +26,14 @@ class BoostUnorderedConan(ConanFile):
                       #assert1 config0 container7 core2 detail5 functional5 iterator5 move3 predef0 preprocessor0 smart_ptr4 throw_exception2 tuple4 type_traits3
 
     def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.source_url))
+        for lib_short_name in self.lib_short_names:
+            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+                     .format(self.version, lib_short_name)) 
 
     def package(self):
-        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="include", src=include_dir)
+        for lib_short_name in self.lib_short_names:
+            include_dir = os.path.join(lib_short_name, "include")
+            self.copy(pattern="*", dst="include", src=include_dir)		
 
     def package_id(self):
         self.info.header_only()
